95bb7f941a043aad01cfba97a43150611464391a
荀徒之 <kalxd@126.com>
2022-02-06 18:30:01 +0800

    略微提高IORef功效。

--- src/FUI/Interal/IORef.ts
+++ src/FUI/Interal/IORef.ts
@@ -4,13 +4,22 @@
 export default class IORef<T> {
 	constructor(private value: T) {}
 
-	write(value: T): T {
+	write(value: T): this {
 		this.value = value;
-		return value;
+		return this;
+	}
+
+	writeWith<K extends keyof T>(key: K, value: T[K]): this {
+		this.value[key] = value;
+		return this;
 	}
 
 	read(): T {
 		return this.value;
+	}
+
+	readWith<K extends keyof T>(key: K): T[K] {
+		return this.value[key];
 	}
 
 	mapOut<R>(f: (ref: T) => R): R {
