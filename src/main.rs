ffbe4af20a3d1ddbf22a0651cdada4b49d53b808
荀涧林 <kalxd@126.com>
2022-03-30 20:47:33 +0800

    一个普通的窗口。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow};
+
+fn gui_main(app: &Application) {
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("我的窗口")
+        .default_width(800)
+        .default_height(600)
+        .build();
+    window.show_all();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let app = Application::builder().application_id("xgley.com").build();
+
+    app.connect_activate(|app| gui_main(app));
+
+    app.run();
 }
