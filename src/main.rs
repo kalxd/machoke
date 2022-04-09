b17356961a4364b25ff18757a12664706de47337
荀涧林 <kalxd@126.com>
2022-04-09 09:39:26 +0800

    自然定律

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
-use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow};
+use gtk::{prelude::*, Entry, Label};
+use gtk::{Application, ApplicationWindow, Box as GtkBox, SizeGroup};
+
+fn create_form_row(label: &str, size_group: &SizeGroup) -> GtkBox {
+    let layout = GtkBox::builder()
+        .orientation(gtk::Orientation::Horizontal)
+        .spacing(10)
+        .build();
+
+    let label = Label::new(Some(&label));
+    size_group.add_widget(&label);
+    layout.pack_start(&label, false, false, 1);
+
+    let entry = Entry::new();
+    layout.pack_start(&entry, true, true, 0);
+
+    return layout;
+}
+
+fn gui_form() -> GtkBox {
+    let layout = GtkBox::builder()
+        .orientation(gtk::Orientation::Vertical)
+        .build();
+
+    let size_group = SizeGroup::new(gtk::SizeGroupMode::Both);
+    let name_row = create_form_row("name", &size_group);
+    let user_row = create_form_row("user", &size_group);
+
+    layout.pack_start(&name_row, false, false, 0);
+    layout.pack_start(&user_row, false, false, 0);
+    return layout;
+}
 
 fn gui_main(app: &Application) {
     let window = ApplicationWindow::builder()
@@ -8,6 +38,16 @@ fn gui_main(app: &Application) {
         .default_width(800)
         .default_height(600)
         .build();
+
+    let main_layout = GtkBox::builder()
+        .orientation(gtk::Orientation::Vertical)
+        .spacing(10)
+        .build();
+
+    let form_layout = gui_form();
+    main_layout.pack_start(&form_layout, false, false, 0);
+
+    window.add(&main_layout);
     window.show_all();
 }
 
