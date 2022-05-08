82d9a355c997b35cc6f5258cf065fda3fd802516
荀涧林 <kalxd@126.com>
2022-05-08 15:50:10 +0800

    重新整理一下。

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,124 @@
+use std::{cell::RefCell, path::PathBuf, rc::Rc};
+
 use gtk::{
-	prelude::*, Application, ApplicationWindow, Box as GtkBox, FileChooserButton, FileFilter, Frame,
+	prelude::*, Application, ApplicationWindow, Box as GtkBox, Button, ButtonBox,
+	FileChooserButton, FileFilter, Frame,
 };
-use widget::FormWidget;
+use widget::{CoverWidget, FormWidget};
 
 mod widget;
 
-fn gui_main(app: &Application) {
-	let window = ApplicationWindow::builder()
-		.application(app)
-		.title("我的窗口")
-		.default_width(800)
-		.default_height(600)
-		.build();
+#[derive(Clone, Debug)]
+struct AppState {
+	audio_path: Rc<RefCell<Option<PathBuf>>>,
+}
 
-	let main_layout = GtkBox::builder()
-		.margin(10)
-		.orientation(gtk::Orientation::Vertical)
-		.spacing(10)
-		.build();
+impl Default for AppState {
+	fn default() -> Self {
+		Self {
+			audio_path: Default::default(),
+		}
+	}
+}
 
-	let file_chooser = FileChooserButton::builder()
-		.title("歌曲")
-		.margin(10)
-		.action(gtk::FileChooserAction::Open)
-		.filter(&{
-			let f = FileFilter::new();
-			f.add_mime_type("audio/*");
-			f
-		})
-		.build();
-	{
-		file_chooser.connect_file_set(move |file_btn| {
-			println!("do this?");
-			dbg!(file_btn.file());
-		});
+struct MainWindow {
+	state: AppState,
+	audio_chooser: FileChooserButton,
+	cover_widget: CoverWidget,
+	form_widget: FormWidget,
+	ok_btn: Button,
+}
+
+impl MainWindow {
+	fn new() -> Self {
+		let state = AppState::default();
+		let audio_chooser = FileChooserButton::builder()
+			.title("歌曲")
+			.margin(10)
+			.action(gtk::FileChooserAction::Open)
+			.filter(&{
+				let f = FileFilter::new();
+				f.add_mime_type("audio/*");
+				f
+			})
+			.build();
+		let cover_widget = widget::CoverWidget::new();
+		let form_widget = FormWidget::new();
+		let ok_btn = Button::builder().label("好").build();
+
+		Self {
+			state,
+			audio_chooser,
+			cover_widget,
+			form_widget,
+			ok_btn,
+		}
 	}
 
-	let frame = Frame::builder().label("选择歌曲").build();
-	frame.add(&file_chooser);
-	main_layout.pack_start(&frame, false, true, 10);
+	fn run(app: &Application) {
+		let main_window = Self::new();
 
-	let cover_widget = widget::CoverWidget::new();
-	let cover_frame = Frame::builder().label("封面设置").build();
-	cover_frame.add(&cover_widget.layout);
-	main_layout.pack_start(&cover_frame, false, false, 10);
+		let window = ApplicationWindow::builder()
+			.application(app)
+			.title("我的窗口")
+			.default_width(800)
+			.default_height(600)
+			.build();
 
-	let frame = Frame::builder().label("属性").build();
-	let form_widget = FormWidget::new();
-	frame.add(&form_widget.layout);
-	main_layout.pack_start(&frame, false, false, 0);
+		let main_layout = GtkBox::builder()
+			.margin(10)
+			.orientation(gtk::Orientation::Vertical)
+			.spacing(10)
+			.build();
 
-	window.add(&main_layout);
-	window.show_all();
+		{
+			let chooser = main_window.audio_chooser.clone();
+			let frame = Frame::builder().label("选择歌曲").build();
+			frame.add(&chooser);
+			main_layout.pack_start(&frame, false, true, 10);
+
+			let audio_path = main_window.state.audio_path.clone();
+			main_window
+				.audio_chooser
+				.clone()
+				.connect_file_set(move |file_btn| {
+					audio_path.replace(file_btn.filename());
+				});
+		}
+
+		{
+			let cover_frame = Frame::builder().label("封面设置").build();
+			cover_frame.add(&main_window.cover_widget.layout);
+			main_layout.pack_start(&cover_frame, false, false, 10);
+		}
+
+		{
+			let frame = Frame::builder().label("属性").build();
+			frame.add(&main_window.form_widget.layout);
+			main_layout.pack_start(&frame, false, false, 0);
+		}
+
+		{
+			let button_box = ButtonBox::builder()
+				.layout_style(gtk::ButtonBoxStyle::End)
+				.build();
+			let state = main_window.state.clone();
+			main_window.ok_btn.clone().connect_clicked(move |_| {
+				dbg!(&state);
+			});
+			button_box.pack_start(&main_window.ok_btn, false, false, 0);
+			main_layout.pack_end(&button_box, false, false, 0);
+		}
+
+		window.add(&main_layout);
+		window.show_all();
+	}
 }
 
 fn main() {
 	let app = Application::builder().application_id("xgley.com").build();
 
-	app.connect_activate(|app| gui_main(app));
+	app.connect_activate(|app| MainWindow::run(app));
 
 	app.run();
 }
