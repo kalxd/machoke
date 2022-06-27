71dfa1ecb2ee7bdda7c63fb904fb978fd065268d
荀涧林 <kalxd@126.com>
2022-06-27 21:19:26 +0800

    第一个窗口。

--- src/widget/mod.rs
+++ src/widget/mod.rs
@@ -1,7 +1,22 @@
-use gtk::{prelude::*, Application};
+use gtk::{prelude::*, Application, ApplicationWindow};
 
-pub struct MainWindow {}
+pub struct MainWindow {
+	window: ApplicationWindow,
+}
 
 impl MainWindow {
-	pub fn run(app: &Application) {}
+	fn new(app: &Application) -> Self {
+		let window = ApplicationWindow::builder()
+			.application(app)
+			.default_height(600)
+			.default_width(800)
+			.build();
+
+		Self { window }
+	}
+
+	pub fn run(app: &Application) {
+		let main_window = Self::new(app);
+		main_window.window.show_all();
+	}
 }
