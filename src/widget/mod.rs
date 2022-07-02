c05562a036557099a4f2b3ef0c0f837546257290
荀涧林 <kalxd@126.com>
2022-07-02 13:14:03 +0800

    消息流通。

--- src/widget/mod.rs
+++ src/widget/mod.rs
@@ -1,17 +1,31 @@
+use gtk::glib;
 use gtk::{prelude::*, Application, ApplicationWindow, Box as GtkBox};
+
+use std::path::PathBuf;
+use std::rc::Rc;
 
 mod cover;
 mod form;
 mod headerbar;
 mod song;
 
+pub(self) enum AppAction {
+	OpenAudia(PathBuf),
+}
+
 pub struct MainWindow {
 	window: ApplicationWindow,
 	widget: song::SongWidget,
+
+	// tx: Rc<glib::Sender<AppAction>>,
+	rx: glib::Receiver<AppAction>,
 }
 
 impl MainWindow {
 	fn new(app: &Application) -> Self {
+		let (tx, rx) = glib::MainContext::channel::<AppAction>(glib::PRIORITY_DEFAULT);
+		let tx = Rc::new(tx);
+
 		let window = ApplicationWindow::builder()
 			.application(app)
 			.default_height(600)
@@ -30,19 +44,34 @@ impl MainWindow {
 		main_layout.pack_start(&form.layout, false, false, 0);
 		window.add(&main_layout);
 
-		title_bar.connect_open_song(|path| {
-			dbg!(path);
+		title_bar.connect_open_song({
+			let tx = tx.clone();
+			move |path| {
+				tx.send(AppAction::OpenAudia(path)).unwrap();
+			}
 		});
 
 		Self {
 			window,
 			widget: form,
+			// tx,
+			rx,
 		}
 	}
 
 	pub fn run(app: &Application) {
 		let main_window = Self::new(app);
+
 		main_window.window.show_all();
 		main_window.widget.hide_something();
+
+		main_window.rx.attach(None, move |msg| {
+			match msg {
+				AppAction::OpenAudia(path) => {
+					dbg!(path);
+				}
+			}
+			glib::Continue(true)
+		});
 	}
 }
