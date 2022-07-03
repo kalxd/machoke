df7d07ed6f56686ed5416e3eaa960b6b94a47a9d
荀涧林 <kalxd@126.com>
2022-07-03 16:07:41 +0800

    流派自动补全。

--- src/widget/form.rs
+++ src/widget/form.rs
@@ -1,7 +1,27 @@
 use gtk::{
-	glib::GString, prelude::*, Box as GtkBox, Entry, Label, Orientation, SizeGroup, SizeGroupMode,
+	glib::GString, prelude::*, Box as GtkBox, Entry, EntryCompletion, Label, ListStore,
+	Orientation, SizeGroup, SizeGroupMode,
 };
 use id3::TagLike;
+
+const GENRE: &[&'static str] = &[
+	"袁派", "王派", "傅派", "戚派", "金派", "张派", "吕派", "徐派", "范派", "陆派", "毕派", "尹派",
+];
+
+struct GenreStore(pub ListStore);
+
+impl GenreStore {
+	fn new() -> Self {
+		let store = ListStore::new(&[gtk::glib::types::Type::STRING]);
+
+		GENRE.iter().for_each(|name| {
+			let iter = store.append();
+			store.set(&iter, &[(0, name)]);
+		});
+
+		Self(store)
+	}
+}
 
 struct FormRow {
 	size_group: SizeGroup,
@@ -26,9 +46,9 @@ impl FormRow {
 		self.size_group.add_widget(&label);
 
 		let entry = Entry::new();
-		row_layout.pack_end(&entry, true, true, 0);
+		row_layout.pack_start(&entry, true, true, 0);
 
-		self.layout.pack_end(&row_layout, false, true, 10);
+		self.layout.pack_start(&row_layout, false, true, 10);
 
 		return entry;
 	}
@@ -59,6 +79,14 @@ impl MetaForm {
 		let artist_entry = form_row.add_row("艺术家");
 		let album_entry = form_row.add_row("专辑");
 		let genre_entry = form_row.add_row("流派");
+
+		let genre_store = GenreStore::new();
+		let genre_completion = EntryCompletion::builder()
+			.model(&genre_store.0)
+			.minimum_key_length(0)
+			.build();
+		genre_entry.set_completion(Some(&genre_completion));
+		genre_completion.set_text_column(0);
 
 		layout.add(&form_row.layout);
 
