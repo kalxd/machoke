0f75a96ca47e82648133d9cda95b63937a14794a
荀洪道 <kalxd@126.com>
2025-03-27 15:24:25 +0800

    一大堆事件。

--- src/value/mod.rs
+++ src/value/mod.rs
@@ -1,1 +1,74 @@
-pub mod parse_box;
+use futures::channel::mpsc;
+use gtk::MessageType;
+use std::{ops::Deref, path::PathBuf};
+
+type AlertMessageBox = (MessageType, String);
+
+pub enum EventAction {
+	OpenAudio((Box<ParseBox>, Option<AlertMessageBox>)),
+	Alert(AlertMessageBox),
+}
+
+pub struct ParseBox {
+	pub audio_tag: id3::Tag,
+	pub audio_src: PathBuf,
+}
+
+impl ParseBox {
+	pub fn parse_from_path(path: PathBuf) -> id3::Result<(Box<Self>, Option<AlertMessageBox>)> {
+		match id3::Tag::read_from_path(&path) {
+			Ok(tag) => Ok((
+				Box::new(Self {
+					audio_src: path,
+					audio_tag: tag,
+				}),
+				None,
+			)),
+			Err(e) if e.partial_tag.is_none() => Ok((
+				Box::new(Self {
+					audio_src: path,
+					audio_tag: id3::Tag::default(),
+				}),
+				Some((
+					MessageType::Warning,
+					String::from("无法解析tag，我亲自为你生成一个！"),
+				)),
+			)),
+			Err(e) => Err(e),
+		}
+	}
+}
+
+#[derive(Clone)]
+pub struct EventSender(mpsc::Sender<EventAction>);
+
+impl EventSender {
+	pub fn send(&self, action: EventAction) {
+		self.0.clone().start_send(action).unwrap();
+	}
+
+	pub fn alert<S: ToString>(&self, t: MessageType, msg: S) {
+		self.send(EventAction::Alert((t, msg.to_string())))
+	}
+
+	pub fn warn<S: ToString>(&self, msg: S) {
+		self.alert(MessageType::Warning, msg)
+	}
+
+	pub fn error<S: ToString>(&self, msg: S) {
+		self.alert(MessageType::Error, msg)
+	}
+}
+
+impl Deref for EventSender {
+	type Target = mpsc::Sender<EventAction>;
+
+	fn deref(&self) -> &Self::Target {
+		&self.0
+	}
+}
+
+pub fn channel() -> (EventSender, mpsc::Receiver<EventAction>) {
+	let (a, b) = mpsc::channel(10);
+	(EventSender(a), b)
+}
--- src/widget/mod.rs
+++ src/widget/mod.rs
@@ -1,4 +1,8 @@
+use std::borrow::BorrowMut;
+
+use futures::{future::ready, SinkExt, StreamExt};
 use gtk::{
+	glib,
 	prelude::{BoxExt, ContainerExt, GtkWindowExt, StackExt, WidgetExt},
 	Application, ApplicationWindow, Box as GtkBox, Stack,
 };
@@ -8,6 +12,8 @@ mod editor;
 mod element;
 mod placeholder;
 mod titlebar;
+
+use crate::value::{self, EventAction, EventSender};
 
 enum StackName {
 	Placeholder,
@@ -29,7 +35,7 @@ pub struct MainWindow {
 }
 
 impl MainWindow {
-	fn new(app: &Application) -> Self {
+	fn new(app: &Application, tx: EventSender) -> Self {
 		let alertbar = alertbar::AlertBar::new();
 
 		let placeholder = placeholder::Placeholder::new();
@@ -59,8 +65,12 @@ impl MainWindow {
 		window.set_titlebar(Some(&*titlebar));
 		window.set_child(Some(&main_layout));
 
-		titlebar.connect_open_audio(move |p| {
-			stack.set_visible_child_name(StackName::Editor.as_str());
+		titlebar.connect_open_audio({
+			let tx = tx.clone();
+			move |p| match value::ParseBox::parse_from_path(p) {
+				Ok(a) => tx.send(EventAction::OpenAudio(a)),
+				Err(e) => tx.error(e),
+			}
 		});
 
 		Self { window, alertbar }
@@ -72,7 +82,25 @@ impl MainWindow {
 	}
 
 	pub fn run(app: &Application) {
-		let main_window = Self::new(app);
+		let (tx, rx) = value::channel();
+
+		let main_window = Self::new(app, tx);
 		main_window.show_all();
+
+		glib::MainContext::default().spawn_local(async move {
+			rx.for_each(|msg| {
+				match msg {
+					EventAction::OpenAudio((t, msg)) => {
+						if let Some(msg) = msg {
+							main_window.alertbar.show(msg.0, msg.1);
+						}
+					}
+					_ => {}
+				};
+
+				ready(())
+			})
+			.await;
+		});
 	}
 }
