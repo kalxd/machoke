f9fd8bb1f990d788101837edab592d6c524138b6
荀洪道 <kalxd@126.com>
2025-10-02 20:34:02 +0800

    全自动CMake处理器。

--- app/src/editor.cc
+++ app/src/editor.cc
@@ -1,9 +1,29 @@
 #include "editor.h"
+#include <QBoxLayout>
+#include <QGroupBox>
+#include <QFormLayout>
 
 namespace XGApp {
 	Editor::Editor(QWidget *parent) : QWidget(parent) { this->setup(); }
 
-    void Editor::setup() {
+    QWidget* Editor::setupBasicForm() {
+		auto widget = new QGroupBox("基本信息");
+        widget->setAlignment(Qt::AlignLeft);
 
+        auto layout = new QFormLayout;
+
+        this->titleLine = new QLineEdit;
+        layout->addRow("标题", this->titleLine);
+
+        widget->setLayout(layout);
+		return widget;
+    }
+
+    void Editor::setup() {
+		auto mainLayout = new QVBoxLayout;
+
+        mainLayout->addWidget(this->setupBasicForm());
+
+        this->setLayout(mainLayout);
     }
 }
--- app/src/editor.h
+++ app/src/editor.h
@@ -2,14 +2,19 @@
 #define _XG_EDITOR_
 
 #include <QWidget>
+#include <QLineEdit>
 
 namespace XGApp {
 	class Editor : public QWidget {
+		Q_OBJECT
     public:
 		explicit Editor(QWidget *parent = nullptr);
 
     private:
+        QWidget* setupBasicForm();
         void setup();
+
+        QLineEdit* titleLine;
 	};
 }
 
