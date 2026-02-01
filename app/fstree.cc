e2c803bb0f9c32ad053e81e2ddeb44c80f411f83
荀洪道 <kalxd@126.com>
2026-02-01 15:32:03 +0800

    简易格式化。

--- app/fstree.cc
+++ app/fstree.cc
@@ -2,8 +2,9 @@
 
 namespace XGApp {
 	FSTree::FSTree(QWidget *parent) {
-        this->dock = new QDockWidget("目录", parent);
+		this->dock = new QDockWidget("目录", parent);
+
         this->dock->setAllowedAreas(Qt::LeftDockWidgetArea);
-        this->dock->setFeatures(QDockWidget::NoDockWidgetFeatures);
-	}
+		this->dock->setFeatures(QDockWidget::NoDockWidgetFeatures);
+    }
 }
